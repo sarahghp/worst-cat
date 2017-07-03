910d0d6e284237aead4e82bd6881790d287baf5c
Sarah GP <sarah.groff.palermo@gmail.com>
2017-07-03 18:43:52 -0400

    Checkpoint

--- src/draw-many-cubes.js
+++ src/draw-many-cubes.js
@@ -3,6 +3,8 @@ window.onload = main;
 
 function main() {
 
+  const I = Immutable;
+
   // create context
   const canvas = document.getElementById('c');
   const gl = initGL(canvas);
@@ -49,12 +51,12 @@ function main() {
     0.0,  100.0,    0.0
   ];
 
-  let position = {
+  let position = I.Map({
     type: 'attribute',
     name: 'a_position',
     data: new Float32Array(vertices),
     pointer: [3, gl.FLOAT, false, 0, 0],
-  };
+  });
 
   const cubeVertexIndices = [
     0,  1,  2,      0,  2,  3,    // front
@@ -65,81 +67,96 @@ function main() {
     20, 21, 22,     20, 22, 23    // left
   ];
 
-  let cubeVertexIndex = {
+  let cubeVertexIndex = I.Map({
     type: 'element_arr',
     name: 'e_indices', // in this case the name is just used in the reconciler as a uniqueID; can gen ID in there if necc. instead
     data: new Uint16Array(cubeVertexIndices),
-  };
-
+  });
   // matrix
-  const initialMatrix = {
+  const initialMatrix = I.Map({
     translation  : [gl.canvas.clientWidth/3, gl.canvas.clientHeight/3, 40],
     rotation     : [1, 1, 1],
     scale        : [1, 1, 1],
-  };
+  });
 
-  let multiMatrix = Array(100).fill(0).map((n) => {
+  let multiMatrixList = I.List(Array(100).fill(0).map((n) => {
     let myMat = Object.assign({}, initialMatrix);
     myMat.translation = [Math.random() * 1000, Math.random() * 1000, Math.random() * 40];
     myMat.rotation = [Math.random() * 7.28, Math.random() * 7.28, 1];
-    return myMat;
-  });
+    myMat.scale = [1, 1, 1];
+    return I.Map(myMat);
+  }));
 
-  let transformMatrix = {
+  let transformMatrix = I.Map({
     type: 'uniform',
     name: 'u_matrix',
     data: [],
     dataType: 'uniformMatrix4fv'
-  };
+  });
 
   // color
   const colors = generateColors();
 
-  let color = {
+  let color = I.Map({
     type: 'attribute',
     name: 'a_color',
     data: new Float32Array(colors),
-    pointer: [4, gl.FLOAT, false, 0, 0],
-  };
+    pointer: [4, gl.FLOAT, false, 0, 0]
+  });
 
   // draw
-  let draw = {
+  let draw = I.Map({
     type: 'draw',
     name: 'd_draw', // in this case the name is just used in the reconciler as a uniqueID; can gen ID in there if necc. instead
     drawCall: gl.drawElements,
     data: [gl.TRIANGLES, 36, gl.UNSIGNED_SHORT, 0]
-  }
+  })
 
   const clientWidth = gl.canvas.clientWidth;
   const clientHeight = gl.canvas.clientHeight;
 
   // animate
-  function animateCube(gl, program, components){
+  function animateCube(gl, program, components, matrixList){
+
     gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 
-    let cubes = multiMatrix.map((mat) => {
-      mat.rotation = mat.rotation.map((r, i) => r += (.01 * i));
+    // have to get updated values, create new transform data, set that data, generate components, call
+
+    // Create matrices
+    const updatedMatrices = matrixList.map((mat, idx) => {
+      return mat.update('rotation', (r) => r += (.01 * idx));
+    });
+
+    const upadtedComponents = updatedMatrices.map((updatedMatrix) => {
+
+      const rotation = updatedMatrix.get('rotation')
+      const translation = updatedMatrix.get('translation');
+      const scale = updatedMatrix.get('scale');
 
       // Compute the matrix
       let matrix = m4.projection(clientWidth, clientHeight, 400);
-          matrix = m4.translate(matrix, mat.translation[0], mat.translation[1], mat.translation[2]);
-          matrix = m4.xRotate(matrix, mat.rotation[0]);
-          matrix = m4.yRotate(matrix, mat.rotation[1]);
-          matrix = m4.zRotate(matrix, mat.rotation[2]);
-          matrix = m4.scale(matrix, mat.scale[0], mat.scale[1], mat.scale[2]);
+          matrix = m4.translate(matrix, translation[0], translation[1], translation[2]);
+          matrix = m4.xRotate(matrix, rotation[0]);
+          matrix = m4.yRotate(matrix, rotation[1]);
+          matrix = m4.zRotate(matrix, rotation[2]);
+          matrix = m4.scale(matrix, scale[0], scale[1], scale[2]);
 
-      myTransform = Object.assign({}, transformMatrix)
-      myTransform.data = [false, matrix];
-
-      return [position, cubeVertexIndex, myTransform, color, draw]
+      return transformMatrix.set('data', [false, matrix]);
     });
 
-    render(gl, program, [].concat(...cubes)); // spread nested return array
-    requestAnimationFrame(animateCube.bind(null, gl, program, components));
+    // Add intro draw sequence
+
+    const updatedSequence = upadtedComponents.flatMap((updatedComp) => {
+      return [components[0], components[1], updatedComp, components[3], components[4]];
+    });
+
+    // Call render & animate
+    render(gl, program, updatedSequence); // spread nested return array
+    // requestAnimationFrame(animateCube.bind(null, gl, program, components, updatedMatrices));
   }
 
   var drawUs = [position, cubeVertexIndex, transformMatrix, color, draw];
-  requestAnimationFrame(animateCube.bind(null, gl, program, drawUs, initialMatrix));
+  requestAnimationFrame(animateCube.bind(null, gl, program, drawUs, multiMatrixList));
 
 }
 
--- src/reconciler.js
+++ src/reconciler.js
@@ -2,22 +2,18 @@ const iMap = Immutable.Map;
 const iList = Immutable.List;
 
 let callOrderDebug = false;
-
-let reconciler = iMap({
-  old: {},
-});
+let reconciler = iMap({});
 
 ////////////////////////////////////////////////////////
 /////////////////////   MAIN   /////////////////////////
 ////////////////////////////////////////////////////////
 
-function render(gl, program, components){
-
-  // let oldKeys = Object.keys(reconciler.old);
+function render(gl, program, components){ // components is a single list of maps
 
   // turn componenets into Immutable List if not already
   let immComponents =  !iList.isList(components) ? iList(components) : components;
 
+  // TODO: figure out how to check with reconciler being mutable
   // check to see if nothing has changed; this will only work if the deep structures
   // have been wrapped as Immutable collections
   if (immComponents.equals(reconciler)) {
@@ -28,15 +24,15 @@ function render(gl, program, components){
   gl.useProgram(program)
 
   // route components
-  let updated = immComponents.map((component, index) => {
+  const updated = immComponents.map((component, index) => {
 
-    switch(component.type) {
+    switch(component.get('type')) {
       case 'attribute':
-        callOrderDebug && console.log(`${component.name} in switch.`);
+        callOrderDebug && console.log(`${component.get('name')} in switch.`);
         return renderAttribute(component, index, program, gl);
 
       case 'uniform':
-        callOrderDebug && console.log(`${component.name} in switch.`);
+        callOrderDebug && console.log(`${component.get('name')} in switch.`);
         return renderUniform(component, index, program, gl);
 
       case 'element_arr':
@@ -49,9 +45,18 @@ function render(gl, program, components){
 
       default:
         console.log(component);
-        console.error('Cannot render component of type:', component.type);
+        console.error('Cannot render component of type:', component.get('type'));
     }
   });
+
+  // this might be interesting to talk about, the choice to use a global state
+  // and also the difference between a value and an identity
+  reconciler = reconciler.withMutations((oldRec) => {
+      oldRec.merge(...updated.filter(Boolean));
+  });
+
+  console.log(reconciler.size);
+
 
   // if(oldKeys.length > updated.length) {
   //   unsetKeys(_.difference(oldKeys, updated));
@@ -63,113 +68,120 @@ function render(gl, program, components){
 ////////////////////////////////////////////////////////
 
 function renderAttribute(component, index, program, gl) {
+
+  if (hasNotChanged(component)) {
+    return null; // this way we can filter it out of the update with a Boolean
+  }
+
+  const componentName = component.get('name');
+  let updatedComponent;
+
+  // if it is new, we do all the things: create location, enable, bind data, then we're done
   if(isNew(component)){
-    // if it is new, we do all the things: create location, enable, bind data, then we're done
-    let location = gl.getAttribLocation(program, component.name);
+    const location = gl.getAttribLocation(program, componentName);
     gl.enableVertexAttribArray(location);
-    component.location = location;
-    component.pointer.unshift(component.location);
-    reconciler.setIn('old', component.name, component);
-    bindAndSetArray(component, gl, gl.ARRAY_BUFFER);
-    callOrderDebug && console.log(component.name + ' render finished');
-    return component.name;
-  }
 
-  // otherwise check if we need to diff and act on that
-  let oldComponent = reconciler.getIn('old', component.name);
+    updatedComponent = component.withMutations((comp) => {
+      // we add the location to the front of the pointer, which is used in the bindAndSetArray call
+      comp.set('location', location)
+          .update('pointer', (p) => p.length < 6 && p.unshift(location));
+    });
 
-  if (oldComponent && (oldComponent.name === component.name) && arraysEqual(oldComponent.data, component.data)){
-    // deep equality check means it is same name & data
   } else {
-    // if it made it through the new check, the location has been set and it exists
-    bindAndSetArray(component, gl, gl.ARRAY_BUFFER);
-    oldComponent = component;
+    updatedComponent = component;
   }
 
-  callOrderDebug && console.log(component.name + ' render finished');
-  return component.name;
+  bindAndSetArray(component, gl, gl.ARRAY_BUFFER);
+  callOrderDebug && console.log(componentName + ' render finished');
+
+  return { [componentName]: updatedComponent };
 }
 
 function renderUniform(component, index, program, gl) {
-  // if it is new, we do all the things: create location, enable, bind data, then we're done
+
+  if (hasNotChanged(component)) {
+    return null; // this way we can filter it out of the update with a Boolean
+  }
+
+  const componentName = component.get('name');
+  let updatedComponent;
+
   if(isNew(component)){
-    let location = gl.getUniformLocation(program, component.name);
-    component.location = location;
-    component.data.unshift(component.location);
-    reconciler.setIn('old', component.name, component);
-    setUniform(component, gl);
-    callOrderDebug && console.log('uniform render finished');
-    return component.name;
-  }
+    const location = gl.getUniformLocation(program, componentName);
 
-  // otherwise check if we need to diff and act on that
-  let oldComponent = reconciler.old[component.name];
+    updatedComponent = component.withMutations((comp) => {
+      // we add the location to the front of the data array, which is used in the setUniform call
+      comp.set('location', location)
+          .update('data', (d) => d.unshift(location))
+    });
 
-  if (oldComponent && (oldComponent.name === component.name) && arraysEqual(oldComponent.data, component.data)){
-    // deep equality check means it is same name & data
   } else {
-    // if it made it through the new check, the location has been set and it exists
-    component.data.unshift(component.location);
-    setUniform(component, gl);
-    oldComponent = component;
+    // overwrite data with location information again
+    // TODO: Improve?
+    updatedComponent = component.update('data', (d) => d.unshift(location));
   }
 
+  setUniform(component, gl);
   callOrderDebug && console.log('uniform render finished');
-  return component.name;
+  return { [componentName]: updatedComponent };
 }
 
-// TODO: Combine with render attribute?
 function renderElementArray(component, index, program, gl) {
+
+  if (hasNotChanged(component)) {
+    return null; // this way we can filter it out of the update with a Boolean
+  }
+
+  const componentName = component.get('name');
+  let updatedComponent;
+
+  // if it is new, we do all the things: create location, enable, bind data, then we're done
   if(isNew(component)){
-    // if it is new, we do all the things: create location, enable, bind data, then we're done
-    let location = gl.getAttribLocation(program, component.name);
-    component.location = location;
-    reconciler.setIn('old', component.name, component);
-    bindAndSetArray(component, gl, gl.ELEMENT_ARRAY_BUFFER);
-    callOrderDebug && console.log('elem render finished');
-    return component.name;
-  }
 
-  // otherwise check if we need to diff and act on that
-  let oldComponent = reconciler.getIn('old', component.name);
+    const location = gl.getAttribLocation(program, componentName);
+    updatedComponent = component.set('location', location);
 
-  if (oldComponent && (oldComponent.name === component.name) && arraysEqual(oldComponent.data, component.data)){
-    // deep equality check means it is same name & data
   } else {
-    // if it made it through the new check, the location has been set and it exists
-    bindAndSetArray(component, gl, gl.ELEMENT_ARRAY_BUFFER);
-    oldComponent = component;
+    updatedComponent = component;
   }
+
+  bindAndSetArray(component, gl, gl.ELEMENT_ARRAY_BUFFER);
   callOrderDebug && console.log('elem render finished');
-  return component.name;
+  return { [componentName]: updatedComponent };
+
 }
 
 function drawIt(component, index, program, gl) {
   // draw is always called
-  reconciler.setIn('old', component.name, component);
-  component.drawCall.apply(gl, component.data);
+  const drawCall = component.get('drawCall');
+  drawCall.apply(gl, component.get('data'));
   callOrderDebug && console.log('draw finished');
 
-  return component.name;
+  // so we don't need to track it
+  return null;
 }
 
 ////////////////////////////////////////////////////////
 ////////////////////   HELPERS   ////////////////////////
 ////////////////////////////////////////////////////////
 
+function hasNotChanged (component) {
+  return component.equals(reconciler.getIn('old', component.get('name')))
+}
+
 function isNew(component) {
-  return !component.hasOwnProperty('location');
+  return !component.has('location');
 }
 
 function bindAndSetArray (component, gl, bufferType){
   let buffer = gl.createBuffer();
   gl.bindBuffer(bufferType, buffer);
-  gl.bufferData(bufferType, component.data, gl.STATIC_DRAW);
-  bufferType === gl.ARRAY_BUFFER && gl.vertexAttribPointer.apply(gl, component.pointer);
+  gl.bufferData(bufferType, component.get('data'), gl.STATIC_DRAW);
+  bufferType === gl.ARRAY_BUFFER && gl.vertexAttribPointer.apply(gl, component.get('pointer'));
 }
 
 function setUniform(component, gl){
-  gl[component.dataType].apply(gl, component.data);
+  gl[component.get('dataType')].apply(gl, component.get('data'));
 }
 
 function arraysEqual (arr1, arr2) {
